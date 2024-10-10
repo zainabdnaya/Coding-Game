293cad1c686acd392d79ad94d79915efd7103f12
zainabdnaya <diyanazizo13@gmail.com>
2024-10-10 10:32:58 +0100

    pr

--- paretheses.go
+++ paretheses.go
@@ -2,37 +2,24 @@
 
 package main
 
-func minAddToMakeValid(s string) int {
-	j := 0
+func minAddToBalanceParentheses(s string) int {
+	left, right := 0, 0
 	
 	for i := 0; i < len(s); i++ {
-		// if s[i] == '(' && (i+1 >= len(s) && s[i+1] != ')') {
-		// 	j += 1
-		// }
-		// if s[i] == ')' && (i-1 < 0 && s[i-1] != '(') {
-		// 	j -= 1
-		// }
-		if s[i] == '(' &&  i+1 < len(s) && s[i+1] == '(' {
-			j += 1
+		if s[i] == '(' {
+			left++
+		} else if s[i] == ')' {
+			if left > 0 {
+				left--
+			} else {
+				right++
+			}
 		}
-		if s[i] == ')' && i+1 < len(s)  && s[i+1] == ')' {
-			j += 1
-		}else {
-		if s[len(s)-1] == '(' && s[len(s)-2] == '('  &&  len(s) - 2 >= 0 {
-			j += 1
-		}
-		if s[len(s) - 1] == ')'  && s[len(s)-2] == ')' && len(s) - 2 >= 0 {
-			j += 1
-		}
-	}	
 	}
-	if j < 0 {
-		j *= -1
-	}
-	return j
+	return left + right
 }
 
 func main() {
 	s := "()))(("
-	print(minAddToMakeValid(s))
+	print(minAddToBalanceParentheses(s))
 }
